d71338968b61b9e542cbef26ad5c3720df02cf05
Lucas Felipe <lucasfelipe3000@gmail.com>
2022-06-05 18:36:07 -0300

    feat: atualizando o projeto com o envio de tweets funcionando na interface

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,43 @@
-import { useState } from 'react'
-import { HeartIcon } from '@heroicons/react/outline'
-import axios from 'axios'
-import { useEffect } from 'react'
+/** @format */
 
-const MAX_TWEET_CHAR = 250
+import { useEffect, useState } from "react";
+import { HeartIcon } from "@heroicons/react/outline";
+import { useFormik } from "formik";
+import axios from "axios";
+
+const MAX_TWEET_CHAR = 240;
 
 // Função formulário do tweet com uso do state(estado)
- function TweetForm() {
-  const [text, setText] = useState('')
+function TweetForm({ loggedInUser, onSuccess }) {
+  const formik = useFormik({
+    onSubmit: async (values, form) => {
+      await axios({
+        method: 'post',
+        url: "http://localhost:9901/tweets",
+        headers: {
+          authorization: `Bearer ${loggedInUser.accessToken}`,
+        },
+        data: {
+          text: values.text,
+        },
+      })
+
+      form.setFieldValue("text", "");
+      onSuccess()
+    },
+    initialValues: {
+      text: "",
+    },
+  })
 
   function changeText(e) {
-    setText(e.target.value)
+    setText(e.target.value);
   }
 
   return (
-  <div className='border-b border-silver p-4 space-y-4'>
-    <div className='flex  space-x-6'>
-       <svg
+    <div className="border-b border-silver p-4 space-y-4">
+      <div className="flex  space-x-6">
+        <svg
           width="35"
           height="35"
           viewBox="0 0 48 48"
@@ -24,77 +45,83 @@ const MAX_TWEET_CHAR = 250
           xmlns="http://www.w3.org/2000/svg"
         >
           <circle cx="24" cy="24" r="24" fill="white" />
-       </svg>
-       <h1 className='font-bold text-xl'>Página Inicial</h1>
-    </div>
-
-  <form className='pl-12 text-lg flex flex-col'>
-      <textarea 
-      name="text"
-      value={text}
-      placeholder="O que está acontecendo?" 
-      className='bg-transparent outline-none disabled:opacity-50'
-      onChange={changeText} 
-      // disabled={true}
-      />
-      
-      <div className='flex justify-end items-center space-x-3'>
-        <span className='text-sm'>  
-            <span> {text.length} </span> / <span className='text-birdBlue'>{MAX_TWEET_CHAR}</span> 
-        </span>
-        <button 
-        className='bg-birdBlue text-platinum px-5 py-2 rounded-full disabled:opacity-50'
-        disabled={text.length > MAX_TWEET_CHAR}
-        >
-        Tweet
-      </button>
+        </svg>
+        <h1 className="font-bold text-xl">Página Inicial</h1>
       </div>
-  </form>
-  </div>
-  )
+
+      <form
+        className="pl-12 text-lg flex flex-col"
+        onSubmit={formik.handleSubmit}
+      >
+        <textarea
+          name="text"
+          value={formik.values.text}
+          placeholder="O que está acontecendo?"
+          className="bg-transparent outline-none disabled:opacity-50"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          disabled={formik.isSubmitting}
+        />
+
+        <div className="flex justify-end items-center space-x-3">
+          <span className="text-sm">
+            <span> {formik.values.text.length} </span> /{" "}
+            <span className="text-birdBlue">{MAX_TWEET_CHAR}</span>
+          </span>
+          <button
+            type="submit"
+            className="bg-birdBlue text-platinum px-5 py-2 rounded-full disabled:opacity-50"
+            disabled={
+              formik.values.text.length > MAX_TWEET_CHAR || formik.isSubmitting
+            }
+          >
+            Tweet
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
- function Tweet({name, username, children}) {
-
+function Tweet({ name, username, children }) {
   return (
     <div className="flex space-x-3 p-4 border-b border-silver">
-       <div>
-         <svg
+      <div>
+        <svg
           width="48"
           height="48"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          >
-          <circle cx="24" cy="24" r="24" fill="#0082DD"/>
-         </svg>
+        >
+          <circle cx="24" cy="24" r="24" fill="#0082DD" />
+        </svg>
       </div>
-      
-      <div className='space-y-1'>
-        <span className="font-bold text-sm">{name}</span> {' '}
-        <span className="text-sm text-silver">@{username}</span>
 
+      <div className="space-y-1">
+        <span className="font-bold text-sm">{name}</span>{" "}
+        <span className="text-sm text-silver">@{username}</span>
         <p>{children}</p>
-        <div className='flex space-x-1 text-silver text-sm items-center'>
-          <HeartIcon className='w-6 stroke-1'/>
+        <div className="flex space-x-1 text-silver text-sm items-center">
+          <HeartIcon className="w-6 stroke-1" />
           <span>1.2k</span>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export function Home({loggedInUser}) {
-   const [data, setData] = useState([])
-  
-   async function getData() {
-    const res = await axios.get('http://localhost:9901/tweets', {
+export function Home({ loggedInUser }) {
+  const [data, setData] = useState([]);
+
+  async function getData() {
+    const res = await axios.get("http://localhost:9901/tweets", {
       headers: {
-        'authorization': `Bearer ${loggedInUser.accessToken}`
+        authorization: `Bearer ${loggedInUser.accessToken}`,
       }
     })
-    setData(res.data)
-   }
+    setData(res.data);
+  }
 
   useEffect(() => {
     getData()
@@ -102,14 +129,19 @@ export function Home({loggedInUser}) {
 
   return (
     <>
-       <TweetForm />
+      <TweetForm loggedInUser={loggedInUser} onSuccess={getData} />
       <div>
-        {data.length && data.map(tweet => (
-          <Tweet key={tweet.id} name={tweet.user.name} username={tweet.user.username}>
-            {tweet.text}
-          </Tweet>          
-        ))}
+        {data.length &&
+          data.map((tweet) => (
+            <Tweet
+              key={tweet.id}
+              name={tweet.user.name}
+              username={tweet.user.username}
+            >
+              {tweet.text}
+            </Tweet>
+          ))}
       </div>
     </>
-  )  
+  );
 }
